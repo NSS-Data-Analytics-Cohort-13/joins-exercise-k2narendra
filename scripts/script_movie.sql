0aaa3dfd71bb5576fec1962a886406d1cee28020
Narendra <kharel.narendra@gmail.com>
2024-10-08 22:25:02 -0400

    final

--- scripts/script_movie.sql
+++ scripts/script_movie.sql
@@ -48,7 +48,8 @@ group by d.company_name;
 
 --5. Write a query that returns the five distributors with the highest average movie budget.
 
-SELECT d.company_name, AVG(r.film_budget) AS Avg_budget
+SELECT d.company_name, 
+	ROUND(AVG(r.film_budget))::MONEY AS Avg_budget
 FROM specs as s
 LEFT JOIN revenue as r
 ON s.movie_id=r.movie_id
@@ -59,9 +60,10 @@ ORDER BY avg_budget DESC
 LIMIT 5
 
 --6. How many movies in the dataset are distributed by a company which is not headquartered in California? Which of these movies has the highest imdb rating?
-SELECT count(s.film_title), max(r.imdb_rating)
+
+SELECT count(s.film_title) AS Number_of_Movie, max(r.imdb_rating) AS Rating
 FROM distributors as d
-LEFT JOIN specs as s
+inner JOIN specs as s
 ON d.distributor_id = s.domestic_distributor_id
 	INNER JOIN rating as r
 	USING(movie_id)
@@ -70,9 +72,12 @@ WHERE d.headquarters NOT IN('%CA%')
 --7. Which have a higher average rating, movies which are over two hours long or movies which are under two hours?
 
 select 
-    avg(case when s.length_in_min <= 120 then r.imdb_rating else 0 end) as under_2hr, 
-    avg(case when s.length_in_min > 120 then r.imdb_rating else 0 end) as over_2hr
+    case when s.length_in_min <= 120 then 'under_2hr' else 'over_2hr' end as movie_length --then r.imdb_rating else 0 end as , then 
+    , avg(r.imdb_rating) as avg_rating
+	--case when s.length_in_min > 120 then r.imdb_rating else 0 end as over_2hr
 from
     rating as r
-join specs as s 
-on s.movie_id = r.movie_id;
+inner join specs as s 
+on s.movie_id = r.movie_id
+	group by movie_length
+order by avg_rating desc 
